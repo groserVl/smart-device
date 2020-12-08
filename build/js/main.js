2336fa92390882bb9baa7563845409ceb3b51bee
groserVl <59569597+groserVl@users.noreply.github.com>
2020-12-08 12:10:36 +0300

    сделаны все блоки, включая модалку(без JS)

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,13 @@
 'use strict';
 
-(function () {})();
+(function () {
+  var buttonSubmit = document.querySelector('button[type="submit"]');
+  var modalTitle = document.querySelector('.modal h2');
+  var modalText = document.querySelector('.modal p');
+  buttonSubmit.textContent = 'Отправить';
+  modalTitle.textContent = 'Закажите звонок';
+  modalText.textContent = 'Оставьте контакты, мы проконсультируем вас бесплатно в удобное время';
+})();
 
 'use strict';
 
--- source/js/modal.js
+++ source/js/modal.js
@@ -1,4 +1,12 @@
 'use strict';
 (function () {
 
+  var buttonSubmit = document.querySelector('button[type="submit"]');
+  var modalTitle = document.querySelector('.modal h2');
+  var modalText = document.querySelector('.modal p');
+
+  buttonSubmit.textContent = 'Отправить';
+  modalTitle.textContent = 'Закажите звонок';
+  modalText.textContent = 'Оставьте контакты, мы проконсультируем вас бесплатно в удобное время';
+
 })();
